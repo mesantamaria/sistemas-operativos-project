3c76cc54435ed0326581d08bccae4948ce9dce85
ANDRES JAHR <asjahr@uc.cl>
2019-05-23 05:47:40 -0400

    Update cr_write

--- src/crfs/funciones/archivos/write.h
+++ src/crfs/funciones/archivos/write.h
@@ -1,3 +1,11 @@
 #pragma once
+#include <stdlib.h>
+#include <stdio.h>
+#include "../generales/bitmap.h"
+#include "../../estructuras.h"
 
-//void cr_bitmap();
+int cr_write(crFILE* file_desc, void* buffer, int nbytes);
+int bitmap_value(unsigned int pointer, FILE* data);
+int condition(FILE* data);
+unsigned int bit_pointer(FILE *data);
+unsigned int index_pointer(FILE* data, crFILE* file_desc);
